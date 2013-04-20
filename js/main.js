3ea4661cfb31f828205f92323c6988e2d68023dd
Aidan Zealley <aidan@zealley.com>
2013-04-20 17:37:37 +0100

    Framework and class efficiency improvements and addition of optional right sidebar

--- js/main.js
+++ js/main.js
@@ -1,36 +1,63 @@
 jQuery(document).ready(function($) {
-	$('body, .window, .side, .page').addClass('scrolling');
-	$('.overthrow').addClass('scrollable');
-	$('.side, .page').addClass('animated');
-	
-	$('.slide').click(function() {
-		$('.side, .page').toggleClass('slid');
-		$('.slide').toggleClass('pressed');
+	$('body').addClass('scrolling');
+	$('.overthrow').addClass('scrollableArea');
+	$('.sidebarLeft, .sidebarRight, .page').addClass('animatedSlide');
+
+	$('.slideRight, .closeLeft').click(function() {
+		$('.sidebarLeft, .page').toggleClass('slidToRight');
+		$('.topBar .slideRight').toggleClass('pressed');
 		return false;
 	});
-	
-		
+
+	$('.slideLeft, .closeRight').click(function() {
+		$('.sidebarRight, .page').toggleClass('slidToLeft');
+		$('.topBar .slideLeft').toggleClass('pressed');
+		return false;
+	});
+
 	$(document).keydown(function(e) {
 		if (e.keyCode === 39) {
-			$('.side, .page').addClass('slid');
-			$('.slide').addClass('pressed');
-			return false;
+			if ($('.sidebarRight, .page').hasClass('slidToLeft')) {
+				$('.sidebarRight, .page').removeClass('slidToLeft');
+				$('.topBar .slideLeft').removeClass('pressed');
+				return false;
+			} else {
+				$('.sidebarLeft, .page').addClass('slidToRight');
+				$('.topBar .slideRight').addClass('pressed');
+				return false;
+			}
 		}
-		
-		if (e.keyCode === 37 && $('.side, .page').hasClass('slid')) {
-			$('.side, .page').removeClass('slid');
-			$('.slide').removeClass('pressed');
-			return false;
+
+		if (e.keyCode === 37) {
+			if ($('.sidebarLeft, .page').hasClass('slidToRight')) {
+				$('.sidebarLeft, .page').removeClass('slidToRight');
+				$('.topBar .slideRight').removeClass('pressed');
+				return false;
+			} else {
+				$('.sidebarRight, .page').addClass('slidToLeft');
+				$('.topBar .slideLeft').addClass('pressed');
+				return false;
+			}
 		}
 	});
-	
-	$('.window').hammer({drag:false, prevent_default:false, css_hacks:false}).on('swipe', function (event) {
+
+	$('body').hammer({drag:false, prevent_default:false, css_hacks:false}).on('swipe', function (event) {
 		if (event.direction === 'right') {
-			$('.side, .page').addClass('slid');
-			$('.slide').addClass('pressed');
+			if ($('.sidebarRight, .page').hasClass('slidToLeft')) {
+				$('.sidebarRight, .page').removeClass('slidToLeft');
+				$('.topBar .slideLeft').removeClass('pressed');
+			} else {
+				$('.sidebarLeft, .page').addClass('slidToRight');
+				$('.topBar .slideRight').addClass('pressed');
+			}
 		} else if (event.direction === 'left') {
-			$('.side, .page').removeClass('slid');
-			$('.slide').removeClass('pressed');
+			if ($('.sidebarLeft, .page').hasClass('slidToRight')) {
+				$('.sidebarLeft, .page').removeClass('slidToRight');
+				$('.topBar .slideRight').removeClass('pressed');
+			} else {
+				$('.sidebarRight, .page').addClass('slidToLeft');
+				$('.topBar .slideLeft').addClass('pressed');
+			}
 		}
 	});
 });
